18849478620241068f7a5180b972373f064e22df
rohantestht <115790284+rohantestht@users.noreply.github.com>
2022-10-18 19:29:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
        console.log('New connection')
-       res.end('Hello Rohan slack test')
+       res.end('Hello Rohan slack uptime test')
 })
 const PORT =  process.env.PORT || 8080
 server.listen(PORT, () => console.log('listening'))
