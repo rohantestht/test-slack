933ed963007ffa21b38bedb4d8953a7d28a331ce
rohantestht <115790284+rohantestht@users.noreply.github.com>
2022-10-19 19:29:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
        console.log('New connection')
-       res.end('testing losg based metrics')
+       res.end('testing 1 logs based metrics')
 })
 const PORT =  process.env.PORT || 8080
 server.listen(PORT, () => console.log('listening'))
